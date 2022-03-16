903091d2beb4b49685d8f96fad08f02d088d7f38
haykam821 <24855774+haykam821@users.noreply.github.com>
2022-03-15 22:49:55 -0400

    Update minigame to Minecraft 1.18.2

--- src/main/java/io/github/haykam821/paintball/Main.java
+++ src/main/java/io/github/haykam821/paintball/Main.java
@@ -4,10 +4,10 @@ import io.github.haykam821.paintball.game.PaintballConfig;
 import io.github.haykam821.paintball.game.item.PaintballItems;
 import io.github.haykam821.paintball.game.phase.PaintballWaitingPhase;
 import net.fabricmc.api.ModInitializer;
-import net.fabricmc.fabric.api.tag.TagFactory;
 import net.minecraft.block.Block;
-import net.minecraft.tag.Tag;
+import net.minecraft.tag.TagKey;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.registry.Registry;
 import xyz.nucleoid.plasmid.game.GameType;
 
 public class Main implements ModInitializer {
@@ -17,7 +17,7 @@ public class Main implements ModInitializer {
 	public static final GameType<PaintballConfig> PAINTBALL_TYPE = GameType.register(PAINTBALL_ID, PaintballConfig.CODEC, PaintballWaitingPhase::open);
 
 	private static final Identifier STAINABLE_TERRACOTTA_ID = new Identifier(MOD_ID, "stainable_terracotta");
-	public static final Tag<Block> STAINABLE_TERRACOTTA = TagFactory.BLOCK.create(STAINABLE_TERRACOTTA_ID);
+	public static final TagKey<Block> STAINABLE_TERRACOTTA = TagKey.of(Registry.BLOCK_KEY, STAINABLE_TERRACOTTA_ID);
 
 	@Override
 	public void onInitialize() {
